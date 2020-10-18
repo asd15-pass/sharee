7eaa2dfe7ec2121d87bb4ec1dce9e348eeda9135
asd15-pass <70442362+asd15-pass@users.noreply.github.com>
2020-10-18 17:50:14 +0900

    投稿時刻の日本時間への修正

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,22 @@
 class MemosController < ApplicationController
   def index
+    @memo = Memo.new
+    @room = Room.find(params[:room_id])
+    @memos = @room.memos.includes(:user)
+  end
+  def create
+    @room = Room.find(params[:room_id])
+    @memos = @room.memos.new(memo_params)
+    if @memos.save
+      redirect_to room_memos_path(@room)
+    else
+      @memo = @room.memo.includes(:user)
+      render :index
+    end
+  end
+  private
+
+  def memo_params
+    params.require(:memo).permit(:content).merge(user_id: current_user.id)
   end
 end
---
+++ app/models/memo.rb
@@ -0,0 +1,4 @@
+class Memo < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
+end
