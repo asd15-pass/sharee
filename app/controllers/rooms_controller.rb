3d69b43c0d0bf61e38814c1ab30de71519593835
asd15-pass <70442362+asd15-pass@users.noreply.github.com>
2020-10-22 18:19:34 +0900

    ベース２

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,11 +16,11 @@ class RoomsController < ApplicationController
   def destroy
     room = Room.find(params[:id])
     room.destroy
-    redirect_to root_path
+    redirect_to rooms_path
   end
     private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name,:destroy ,user_ids: [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'top/index'
-  devise_for :users
-  root to: "rooms#index"
+  devise_for :users, controllers: {   registrations: 'users/registrations',
+    sessions: 'users/sessions' }
+  root to: "top#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create, :destroy] do
+  resources :rooms, only: [:new,:index, :create, :destroy] do
     resources :memos, only: [:index, :create]
   end
 end
